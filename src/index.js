6893766035581d88d866c924268eb73111f5c6c4
denis-tsibaev <denis.tsibaev@gmail.com>
2023-03-19 23:49:29 +0700

    debounce
    добавил lodash.debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const debounce = require('lodash.debounce');
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+inputEl.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
