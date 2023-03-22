dee5ef1805fcb63e6b3404d72b2a33dd57a99949
denis-tsibaev <denis.tsibaev@gmail.com>
2023-03-23 00:18:05 +0700

    qwe

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
-export default function fetchCountries(name) {
-  const url = 'https://restcountries.com/v3.1/name';
-  fetch(url)
-    .then(r => r.json)
-    .then(console.log);
-}
+// export default function fetchCountries(name) {
+//   const url = `https://restcountries.com/v3.1/${name}`;
+//   fetch(url)
+//     .then(r => r.json)
+//     .then(console.log);
+// }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 const debounce = require('lodash.debounce');
 import './css/styles.css';
-import fetchCountries from './fetchCountries';
+// import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
 inputEl.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+
+const infoDiv = document.querySelector('.country-info');
+
+function fetchCountries(e) {
+  const countryName = e.target.value.trim();
+  console.log(countryName);
+  const url = `https://restcountries.com/v3.1/name/${countryName}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+  console.log(data);
+}
+
+// function render(query) {
+//   console.log(query.capital);
+// }
+
+//sweden
