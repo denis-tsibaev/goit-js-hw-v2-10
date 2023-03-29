6183fe011e63370ef995791bf652e3da63155b05
denis-tsibaev <denis.tsibaev@gmail.com>
2023-03-29 23:49:08 +0700

    Сделал работу. Осталось причесать

--- src/index.js
+++ src/index.js
@@ -2,36 +2,59 @@ const debounce = require('lodash.debounce');
 import './css/styles.css';
 // import fetchCountries from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
 
-const inputEl = document.querySelector('#search-box');
+const inputEl = document.querySelector('input#search-box');
 inputEl.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
 
 const infoDiv = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
 function fetchCountries(e) {
+  countryList.innerHTML = '';
+  infoDiv.innerHTML = '';
   const countryName = e.target.value.trim();
   console.log(countryName);
   const url = `https://restcountries.com/v3.1/name/${countryName}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
+      if (data.status === 404) {
+        alert('Некорректный запрос');
+        return;
+      }
+      if (data.length > 10) {
+        alert('Слишком много вариантов. Делайте запрос точнее');
+        return;
+      }
       console.log(data);
-      const country = data[0];
-      const langs = Object.values(data[0].languages).join(', ');
-      console.log('population: ', data[0].population);
-      console.log('name official: ', data[0].name.official);
-      console.log('capital: ', data[0].capital);
-      console.log('languages: ', data[0].languages);
-      infoDiv.innerHTML = `
-	  <p>name official: ${country.name.official}</p>
-	  <p>population: ${country.population}</p>
-	  <p>capital: ${country.capital}</p>
-	  <img width=320 src="${country.flags.svg}" alt ="${country.flags.alt}"/>
-	  <p>languages: ${langs}</p>
-		`;
-    })
-    .catch(error => console.log(error));
-}
+      for (const country of data) {
+        console.log(country);
+        countryList.insertAdjacentHTML(
+          'beforeend',
+          `
+		 <li class= "item-list">
+		 	<img width=50 src="${country.flags.svg}" alt ="${country.flags.alt}"/>
+			<p class="country-name">${country.name.official}</p>
+		 </li> 
+		  `
+        );
+      }
 
-//sweden
+      if (data.length === 1) {
+        const country = data[0];
+        const langs = Object.values(data[0].languages).join(', ');
+        countryList.innerHTML = '';
+        infoDiv.innerHTML = `
+			<img width=200 src="${country.flags.svg}" alt ="${country.flags.alt}"/> 
+			<p class="title-name">${country.name.official}</p>
+			<p><span class="title-span">capital</span>: ${country.capital}</p>
+			<p><span class="title-span">population:</span> ${country.population}</p>
+			<p><span class="title-span">languages:</span> ${langs}</p>
+			`;
+      }
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+}
